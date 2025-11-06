293ebdc23c1a7098ced56c72466b100982884951
Jorge Reyes <Jorge.Reyes@fiscal.treasury.gov>
2025-11-06 07:47:52 -0500

    Fix SQL syntax - remove invalid USAGE on DATABASE

--- run_this_first.sql
+++ run_this_first.sql
@@ -2,7 +2,6 @@
 
 CREATE USER "IAM:redshift-masking-automation-role-sxo4h3gr" PASSWORD DISABLE;
 GRANT ROLE sys:secadmin TO "IAM:redshift-masking-automation-role-sxo4h3gr";
-GRANT USAGE ON DATABASE dev TO "IAM:redshift-masking-automation-role-sxo4h3gr";
 GRANT USAGE ON SCHEMA public TO "IAM:redshift-masking-automation-role-sxo4h3gr";
 GRANT SELECT ON information_schema.columns TO "IAM:redshift-masking-automation-role-sxo4h3gr";
 GRANT SELECT ON pg_user TO "IAM:redshift-masking-automation-role-sxo4h3gr";
